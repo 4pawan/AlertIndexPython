b7f7ef10ab235df4d6db34daa1a5674a2e4f7483
Pawan Singh <pwn.singh14@gmail.com>
2024-03-20 23:14:10 +0530

    init commit

--- smartApi_timer/notify_user.py
+++ smartApi_timer/notify_user.py
@@ -9,8 +9,9 @@ class NotifyUser:
     def send_message(data: Result):
           message = ''
           if not (data is None):
-              strgth = f"<b>{data.Strength}</b>" if data.Strength == 0 else data.Strength         
-              tu.send_telegram_message(f"{data.Signal}{strgth}  {data.Change}  close {data.Close} oi  {data.OI}   vol {data.Vol}  rsi {data.Rsi} {data.Mv9-data.Mv26}",config.telegram_token,config.telegram_chat_id)
+              strgth = f"<b>{data.Strength}</b>" if data.Strength == 0 else data.Strength  
+              mv_diff = float("{:.2f}".format(data.Mv9-data.Mv26))      
+              tu.send_telegram_message(f"{data.Signal}{strgth}  {data.Change}  close {data.Close} oi  {data.OI}   vol {data.Vol}  rsi {data.Rsi} {mv_diff}",config.telegram_token,config.telegram_chat_id)
               message = f"{data.Date}|{data.Signal}|{data.Strength}|{data.Close}|{data.Change}|{data.OI}|{data.Vol}|{data.Rsi}|"
               bu.add_logging_to_azure_blob(message+"\n")
           else:
